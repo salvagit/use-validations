2491d1acb3d349ad00cf40a0b44e62e01d6d4ede
salva <s@salva.io>
2023-06-24 14:17:15 -0300

    chore: remove dist folder.

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import useValidations from './useValidations';
-export default useValidations;
-export * from './useValidations';
-export * from './types';
-export * from './validators';
--- dist/validators.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const isRequired: (value: string) => "required" | null;
